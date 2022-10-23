f802e1532736045679d248d3f1221efd0e17831e
SharkFishDeveloper <shahzebakhtar66@gmail.com>
2022-10-23 16:46:35 +0530

    partially designed add note screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'screens/insert_note_screen.dart';
 import 'screens/query_screen.dart';
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.pink,
+          accentColor: Colors.pinkAccent[100]
+    ),
       routes: {
         '/':(context)=>InsertNoteScreen(),
         '/query':(context)=>QueryScreen()
--- lib/screens/insert_note_screen.dart
+++ lib/screens/insert_note_screen.dart
@@ -7,8 +7,40 @@ class InsertNoteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add Your Note"),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: Colors.pinkAccent[500],
+        centerTitle: true,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            "Add Your Note ",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.note_add))
+        ]),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                icon: CircleAvatar(child: Icon(Icons.add),
+                backgroundColor: Colors.pink[100],),
+                hintText: "Title",
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: Text("Done")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
+// TODO to add 2 text input fields
